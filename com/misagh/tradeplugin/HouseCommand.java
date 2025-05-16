7ceed4419df9c7ea35dca5efacfc6c9c281c9a31
Misagh <151332741+Misagh021kid@users.noreply.github.com>
2025-05-16 10:45:01 +0330

    Recoded and Added Docs

--- com/misagh/tradeplugin/HouseCommand.java
+++ com/misagh/tradeplugin/HouseCommand.java
@@ -3,10 +3,20 @@ package com.misagh.tradeplugin;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+
+/**
+ * Advanced Region Market API Imports
+ */
 import net.alex9849.arm.AdvancedRegionMarket;
 import net.alex9849.arm.regions.Region;
 import net.alex9849.arm.regions.SellRegion;
+/**
+ * Vault API imports
+ */
 import net.milkbowl.vault.economy.Economy;
+/**
+ * Bukkit API imports
+ */
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -15,142 +25,149 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+/**
+ * Handles the /sellhouse and /buyhouse commands.
+ */
 public class HouseCommand implements CommandExecutor {
    private final Economy economy;
-   private static final Map<UUID, HouseCommand.PendingSale> pendingSales = new HashMap();
-   private final Map<UUID, Long> playerCooldowns = new HashMap();
+   private static final Map<UUID, PendingSale> pendingSales = new HashMap<>();
 
    public HouseCommand(Economy economy) {
       this.economy = economy;
    }
 
+   @Override
    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
       if (!(sender instanceof Player)) {
-         sender.sendMessage(ChatColor.RED + "In dastor faghat baraye player hast!");
+         sender.sendMessage(ChatColor.RED + "Only players can use this command.");
          return true;
-      } else {
-         Player player = (Player)sender;
-         if (label.equalsIgnoreCase("sellhouse")) {
-            return this.handleSellHouse(player, args);
-         } else {
-            return label.equalsIgnoreCase("buyhouse") ? this.handleBuyHouse(player, args) : false;
-         }
       }
+
+      Player player = (Player) sender;
+      if (label.equalsIgnoreCase("sellhouse")) {
+         return handleSellHouse(player, args);
+      } else if (label.equalsIgnoreCase("buyhouse")) {
+         return handleBuyHouse(player, args);
+      }
+      return false;
    }
 
+   /**
+    * Handles The SellHouse Command.
+    */
    private boolean handleSellHouse(final Player player, String[] args) {
       if (args.length != 3) {
-         player.sendMessage(ChatColor.RED + "Estefade: /sellhouse <player> <esm region> <geymat>");
+         player.sendMessage(ChatColor.RED + "Usage: /sellhouse <player> <regionName> <price>");
          return true;
-      } else {
-         final Player targetPlayer = Bukkit.getPlayer(args[0]);
-         final String regionName = args[1];
-
-         double sellPrice;
-         try {
-            sellPrice = Double.parseDouble(args[2]);
-         } catch (NumberFormatException var15) {
-            player.sendMessage(ChatColor.RED + "Gheymat bayad adad bashe!");
-            return true;
-         }
-
-         if (targetPlayer != null && targetPlayer.isOnline()) {
-            Region region = AdvancedRegionMarket.getInstance().getRegionManager().getRegionByNameAndWorld(regionName, player.getWorld().getName());
-            if (region == null) {
-               player.sendMessage(ChatColor.RED + "Region mored nazar peyda nashod!");
-               return true;
-            } else if (!(region instanceof SellRegion)) {
-               player.sendMessage(ChatColor.RED + "In region baraye foroosh ghabele estefade nist!");
-               return true;
-            } else {
-               SellRegion sellRegion = (SellRegion)region;
-               if (!player.getUniqueId().equals(sellRegion.getOwner())) {
-                  player.sendMessage(ChatColor.RED + "Shoma malek in region nistid!");
-                  return true;
-               } else {
-                  double originalPrice = sellRegion.getPriceObject().calcPrice(sellRegion.getRegion());
-                  double minPrice = originalPrice / 2.0D;
-                  if (!(sellPrice < minPrice) && !(sellPrice > originalPrice)) {
-                     pendingSales.put(player.getUniqueId(), new HouseCommand.PendingSale(sellRegion, targetPlayer.getUniqueId(), sellPrice));
-                     Bukkit.getLogger().info("PendingSales after registration: " + pendingSales.toString());
-                     (new BukkitRunnable() {
-                        public void run() {
-                           if (HouseCommand.pendingSales.containsKey(player.getUniqueId()) && ((HouseCommand.PendingSale)HouseCommand.pendingSales.get(player.getUniqueId())).isExpired()) {
-                              HouseCommand.pendingSales.remove(player.getUniqueId());
-                              player.sendMessage(ChatColor.RED + "Pishnahad foroosh region '" + regionName + "' shoma montaghi shod.");
-                              ChatColor var10001 = ChatColor.RED;
-                              targetPlayer.sendMessage(var10001 + "Pishnahad foroosh region '" + regionName + "' az player " + player.getName() + " montaghi shod.");
-                           }
-
-                           Bukkit.getLogger().info("PendingSales after task removal: " + HouseCommand.pendingSales.toString());
-                        }
-                     }).runTaskLater(TradePlugin.getInstance(), 1200L);
-                     ChatColor var10001 = ChatColor.GREEN;
-                     player.sendMessage(var10001 + "Shoma pishnahad foroosh region '" + regionName + "' ro be player " + targetPlayer.getName() + " sabt kardid! Gheymat: " + sellPrice);
-                     var10001 = ChatColor.YELLOW;
-                     targetPlayer.sendMessage(var10001 + "Player " + player.getName() + " pishnahad foroosh region '" + regionName + "' ro be shoma dade ast! Gheymat: " + sellPrice);
-                     return true;
-                  } else {
-                     player.sendMessage(ChatColor.RED + "Gheymat bayad beyn " + minPrice + " va " + originalPrice + " bashe!");
-                     return true;
-                  }
-               }
-            }
-         } else {
-            player.sendMessage(ChatColor.RED + "Player mored nazar online nist ya peyda nashod!");
-            return true;
-         }
       }
+
+      final Player targetPlayer = Bukkit.getPlayer(args[0]);
+      final String regionName = args[1];
+      double sellPrice;
+
+      try {
+         sellPrice = Double.parseDouble(args[2]);
+      } catch (NumberFormatException e) {
+         player.sendMessage(ChatColor.RED + "Price must be a valid number.");
+         return true;
+      }
+
+      if (targetPlayer == null || !targetPlayer.isOnline()) {
+         player.sendMessage(ChatColor.RED + "Target player is not online or not found.");
+         return true;
+      }
+
+      Region region = AdvancedRegionMarket.getInstance().getRegionManager().getRegionByNameAndWorld(regionName, player.getWorld().getName());
+      if (region == null) {
+         player.sendMessage(ChatColor.RED + "Specified region not found.");
+         return true;
+      }
+
+      if (!(region instanceof SellRegion)) {
+         player.sendMessage(ChatColor.RED + "This region is not a sellable region.");
+         return true;
+      }
+
+      SellRegion sellRegion = (SellRegion) region;
+      if (!player.getUniqueId().equals(sellRegion.getOwner())) {
+         player.sendMessage(ChatColor.RED + "You are not the owner of this region.");
+         return true;
+      }
+
+      double originalPrice = sellRegion.getPriceObject().calcPrice(sellRegion.getRegion());
+      double minPrice = originalPrice / 2.0;
+
+      if (sellPrice < minPrice || sellPrice > originalPrice) {
+         player.sendMessage(ChatColor.RED + "Price must be between " + minPrice + " and " + originalPrice + ".");
+         return true;
+      }
+
+      pendingSales.put(player.getUniqueId(), new PendingSale(sellRegion, targetPlayer.getUniqueId(), sellPrice));
+      new BukkitRunnable() {
+         @Override
+         public void run() {
+            PendingSale pending = pendingSales.get(player.getUniqueId());
+            if (pending != null && pending.isExpired()) {
+               pendingSales.remove(player.getUniqueId());
+               player.sendMessage(ChatColor.RED + "Your region sale offer for '" + regionName + "' has expired.");
+               targetPlayer.sendMessage(ChatColor.RED + "The offer from " + player.getName() + " for region '" + regionName + "' has expired.");
+            }
+         }
+      }.runTaskLater(TradePlugin.getInstance(), 1200L);
+
+      player.sendMessage(ChatColor.GREEN + "You have offered region '" + regionName + "' to " + targetPlayer.getName() + " for $" + sellPrice);
+      targetPlayer.sendMessage(ChatColor.YELLOW + player.getName() + " has offered to sell region '" + regionName + "' to you for $" + sellPrice);
+      return true;
    }
 
+   /**
+    * Handles The BuyHouse Command.
+    */
    private boolean handleBuyHouse(Player player, String[] args) {
       if (args.length != 1) {
-         player.sendMessage(ChatColor.RED + "Estefade: /buyhouse <player>");
+         player.sendMessage(ChatColor.RED + "Usage: /buyhouse <seller>");
          return true;
-      } else {
-         Player seller = Bukkit.getPlayer(args[0]);
-         if (seller != null && seller.isOnline()) {
-            Bukkit.getLogger().info("PendingSales Map: " + pendingSales.toString());
-            Bukkit.getLogger().info("Looking for seller UUID: " + seller.getUniqueId());
-            if (!pendingSales.containsKey(seller.getUniqueId())) {
-               Bukkit.getLogger().info("Seller UUID not found in pendingSales: " + seller.getUniqueId());
-               player.sendMessage(ChatColor.RED + "In player hich regioni baraye foroosh sabt nakarde ast!");
-               return true;
-            } else {
-               HouseCommand.PendingSale pendingSale = (HouseCommand.PendingSale)pendingSales.get(seller.getUniqueId());
-               if (pendingSale.isExpired()) {
-                  pendingSales.remove(seller.getUniqueId());
-                  player.sendMessage(ChatColor.RED + "In pishnahad foroosh montaghi shode ast!");
-                  return true;
-               } else if (!pendingSale.getBuyerId().equals(player.getUniqueId())) {
-                  player.sendMessage(ChatColor.RED + "In region be player digar pishnahad shode ast!");
-                  return true;
-               } else {
-                  SellRegion sellRegion = pendingSale.getSellRegion();
-                  double sellPrice = pendingSale.getPrice();
-                  if (!this.economy.has(player, sellPrice)) {
-                     player.sendMessage(ChatColor.RED + "Shoma pol kafi baraye kharid in region nadarin! Gheymat: " + sellPrice);
-                     return true;
-                  } else {
-                     this.economy.withdrawPlayer(player, sellPrice);
-                     this.economy.depositPlayer(seller, sellPrice);
-                     sellRegion.setOwner(Bukkit.getOfflinePlayer(player.getUniqueId()));
-                     pendingSales.remove(seller.getUniqueId());
-                     ChatColor var10001 = ChatColor.GREEN;
-                     player.sendMessage(var10001 + "Shoma region '" + sellRegion.getRegion().getId() + "' ra ba movafaghiyat az " + seller.getName() + " kharidid! Gheymat: " + sellPrice);
-                     var10001 = ChatColor.GREEN;
-                     seller.sendMessage(var10001 + "Region shoma be player " + player.getName() + " forookhte shod! Gheymat: " + sellPrice);
-                     return true;
-                  }
-               }
-            }
-         } else {
-            player.sendMessage(ChatColor.RED + "Seller online nist ya peyda nashod!");
-            return true;
-         }
       }
+
+      Player seller = Bukkit.getPlayer(args[0]);
+      if (seller == null || !seller.isOnline()) {
+         player.sendMessage(ChatColor.RED + "Seller is not online or not found.");
+         return true;
+      }
+
+      PendingSale pendingSale = pendingSales.get(seller.getUniqueId());
+      if (pendingSale == null || pendingSale.isExpired()) {
+         pendingSales.remove(seller.getUniqueId());
+         player.sendMessage(ChatColor.RED + "There is no active sale offer from this player.");
+         return true;
+      }
+
+      if (!pendingSale.getBuyerId().equals(player.getUniqueId())) {
+         player.sendMessage(ChatColor.RED + "This sale offer was not made to you.");
+         return true;
+      }
+
+      SellRegion sellRegion = pendingSale.getSellRegion();
+      double sellPrice = pendingSale.getPrice();
+
+      if (!economy.has(player, sellPrice)) {
+         player.sendMessage(ChatColor.RED + "You do not have enough money to buy this region. Price: $" + sellPrice);
+         return true;
+      }
+
+      economy.withdrawPlayer(player, sellPrice);
+      economy.depositPlayer(seller, sellPrice);
+      sellRegion.setOwner(Bukkit.getOfflinePlayer(player.getUniqueId()));
+      pendingSales.remove(seller.getUniqueId());
+
+      player.sendMessage(ChatColor.GREEN + "You have successfully purchased region '" + sellRegion.getRegion().getId() + "' from " + seller.getName() + " for $" + sellPrice);
+      seller.sendMessage(ChatColor.GREEN + "You have sold your region to " + player.getName() + " for $" + sellPrice);
+      return true;
    }
 
+   /**
+    * Represents a Pending Region Sale.
+    */
    private static class PendingSale {
       private final SellRegion sellRegion;
       private final UUID buyerId;
@@ -165,19 +182,19 @@ public class HouseCommand implements CommandExecutor {
       }
 
       public SellRegion getSellRegion() {
-         return this.sellRegion;
+         return sellRegion;
       }
 
       public UUID getBuyerId() {
-         return this.buyerId;
+         return buyerId;
       }
 
       public double getPrice() {
-         return this.price;
+         return price;
       }
 
       public boolean isExpired() {
-         return System.currentTimeMillis() > this.expirationTime;
+         return System.currentTimeMillis() > expirationTime;
       }
    }
 }
--- com/misagh/tradeplugin/TradePlugin.java
+++ com/misagh/tradeplugin/TradePlugin.java
@@ -1,49 +1,131 @@
+/**
+ * ================================
+ *      ReginTrader - Minecraft
+ * ================================
+ *
+ * Plugin Name:    RegionTrader
+ * Author:        Misagh021Kid (f35j#0000)
+ * Version:        1.0
+ * Created:        2025-05-16
+ *
+ * GitHub:         https://github.com/Misagh021kid/RegionTrader
+ * Description:
+ *      A Minecraft plugin that allows players to buy and sell houses
+ *      using Vault economy systems.
+ *
+ * Dependencies:
+ *      - Vault (required)
+ *      - Any Economy plugin (e.g., EssentialsX Economy)
+ *
+ * Commands:
+ *      - /sellhouse -> Sell a house to another player
+ *      - /buyhouse  -> Buy a house from another player
+ *
+ *
+ */
+
 package com.misagh.tradeplugin;
 
-import java.util.Objects;
+/**
+ * Vault imports for integrating the economy system
+ */
 import net.milkbowl.vault.economy.Economy;
+
+/**
+ * Bukkit API imports
+ */
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Main plugin class for TradePlugin
+ */
 public class TradePlugin extends JavaPlugin {
-   private static Economy economy = null;
+   /**
+    * Static instance of the economy provider
+    */
+   private static Economy economy;
+
+   /**
+    * Static instance of the plugin
+    */
    private static TradePlugin instance;
 
+   /**
+    * Called when the plugin is enabled
+    */
+   @Override
    public void onEnable() {
       instance = this;
-      if (!this.setupEconomy()) {
-         this.getLogger().severe("Vault پیدا نشد! پلاگین غیرفعال خواهد شد.");
-         this.getServer().getPluginManager().disablePlugin(this);
-      } else {
-         ((PluginCommand)Objects.requireNonNull(this.getCommand("sellhouse"))).setExecutor(new HouseCommand(economy));
-         ((PluginCommand)Objects.requireNonNull(this.getCommand("buyhouse"))).setExecutor(new HouseCommand(economy));
-         ((PluginCommand)Objects.requireNonNull(this.getCommand("tradehouse"))).setExecutor(new TradeCommand(economy));
-         ((PluginCommand)Objects.requireNonNull(this.getCommand("tradeaccept"))).setExecutor(new TradeCommand(economy));
-         ((PluginCommand)Objects.requireNonNull(this.getCommand("tradeDecline"))).setExecutor(new TradeCommand(economy));
-         this.getLogger().info("پلاگین با موفقیت فعال شد!");
+
+      if (!setupEconomy()) {
+         getLogger().severe("Vault dependency not found! Disabling plugin...");
+         getServer().getPluginManager().disablePlugin(this);
+         return;
       }
+
+      registerCommand("sellhouse", new HouseCommand(economy));
+      registerCommand("buyhouse", new HouseCommand(economy));
+
+      getLogger().info("TradePlugin enabled! (Author: f35j#0000)");
    }
 
+   /**
+    * Called when the plugin is disabled
+    */
+   @Override
    public void onDisable() {
-      this.getLogger().info("پلاگین غیرفعال شد.");
+      getLogger().info("TradePlugin disabled. Hope you enjoyed it!");
    }
 
+   /**
+    * Gets the plugin instance
+    * @return instance of TradePlugin
+    */
    public static TradePlugin getInstance() {
       return instance;
    }
 
+   /**
+    * Gets the economy provider
+    * @return economy instance
+    */
+   public static Economy getEconomy() {
+      return economy;
+   }
+
+   /**
+    * Initializes The Economy Provider With Vault
+    * @return true if economy was successfully set up, false otherwise
+    */
    private boolean setupEconomy() {
-      if (this.getServer().getPluginManager().getPlugin("Vault") == null) {
+      if (getServer().getPluginManager().getPlugin("Vault") == null) {
          return false;
-      } else {
-         RegisteredServiceProvider<Economy> rsp = this.getServer().getServicesManager().getRegistration(Economy.class);
-         if (rsp == null) {
-            return false;
-         } else {
-            economy = (Economy)rsp.getProvider();
-            return true;
-         }
       }
+
+      RegisteredServiceProvider<Economy> rsp = getServer()
+              .getServicesManager()
+              .getRegistration(Economy.class);
+
+      if (rsp == null) {
+         return false;
+      }
+
+      economy = rsp.getProvider();
+      return economy != null;
+   }
+
+   /**
+    * Registers a Command and Assigns Executor
+    * @param name command name
+    * @param executor command executor
+    */
+   private void registerCommand(String name, HouseCommand executor) {
+      PluginCommand cmd = getCommand(name);
+      if (cmd == null) {
+         return;
+      }
+      cmd.setExecutor(executor);
    }
 }
